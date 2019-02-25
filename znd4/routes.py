2cf2c4cebd09f32874768b92d96e26c7a946b354
Zane Dufour <zane.dufour@gmail.com>
2019-02-24 21:28:22 -0500

    Got 'website' working with multiple pagesgit status!

--- znd4/routes.py
+++ znd4/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from znd4 import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Zane'}
+    return render_template('index.html', title='Home')
+
+@app.route('/resume')
+def resume():
+    user = {'username': 'Zane'}
+    return render_template('resume.html', title='Resume')
+
+@app.route('/blog')
+def blog():
+    user = {'username': 'Zane'}
+    return render_template('blog.html', title='Blog')
     
