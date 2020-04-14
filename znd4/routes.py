dc82e24633b2455cad080830e23e8638491ef29c
Zane DuFour <zane.dufour@gmail.com>
2020-04-13 22:00:30 -0400

    run black check before pylint

--- znd4/routes.py
+++ znd4/routes.py
@@ -4,29 +4,31 @@ This is where we define URLs (routes)
 from flask import render_template
 from znd4 import app
 
-@app.route('/')
-@app.route('/index')
+
+@app.route("/")
+@app.route("/index")
 def index():
     """The homepage"""
-    return render_template('index.html', title='Home')
+    return render_template("index.html", title="Home")
 
-@app.route('/resume')
+
+@app.route("/resume")
 def resume():
     """The link to my resume"""
-    return render_template('resume.html', title='Resume')
+    return render_template("resume.html", title="Resume")
 
-@app.route('/blog')
+
+@app.route("/blog")
 def blog():
     """The splash page for my blog"""
     posts = [
         {
-            'title': 'Beautiful, Awesome Animals',
-            'content': 'A tale of cute, furry doggos.',
+            "title": "Beautiful, Awesome Animals",
+            "content": "A tale of cute, furry doggos.",
         },
         {
-            'title': 'Making Games Matter',
-            'content': 'How we make a website with real blog posts',
+            "title": "Making Games Matter",
+            "content": "How we make a website with real blog posts",
         },
     ]
-    return render_template('blog.html', title='Blog', posts=posts)
-
+    return render_template("blog.html", title="Blog", posts=posts)
