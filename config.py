5a729c2b029992e33a5e8998a5cc782f20605747
Zane DuFour <zane.dufour@gmail.com>
2020-04-13 22:02:15 -0400

    run black

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
--- znd4/tests/test_models.py
+++ znd4/tests/test_models.py
@@ -1,4 +1,5 @@
 """Test the backend models module"""
 
+
 def test_foo():
     """A fake test, because I just want to get nox to pass"""
