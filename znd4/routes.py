4b5c230d34da9c75c9438f53b069e3700711aa92
Zane DuFour <zane.dufour@gmail.com>
2020-04-13 21:36:56 -0400

    removed unnecessary user= lines

--- znd4/routes.py
+++ znd4/routes.py
@@ -4,12 +4,10 @@ from znd4 import app
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Zane'}
     return render_template('index.html', title='Home')
 
 @app.route('/resume')
 def resume():
-    user = {'username': 'Zane'}
     return render_template('resume.html', title='Resume')
 
 @app.route('/blog')
