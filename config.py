1704a41793265d3d970594a70181858ad14c1a59
Zane DuFour <zane.dufour@gmail.com>
2020-04-14 22:14:44 -0400

    add SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
 import os
 from pathlib import Path
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = Path(__file__).resolve().parent
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
+    SECRET_KEY = os.getenv("SECRET_KEY") or "you-will-never-guess"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or "sqlite:///" + (
+        basedir / "app.db"
+    )
