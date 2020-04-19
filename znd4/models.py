ec7003644f02d1ca8d469bc799876c097ce28f9d
Zane DuFour <zane.dufour@gmail.com>
2020-04-19 17:22:21 -0400

    remove primary_key=True to body

--- znd4/models.py
+++ znd4/models.py
@@ -15,6 +15,6 @@ class User(db.Model):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140), primary_key=True)
+    body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
