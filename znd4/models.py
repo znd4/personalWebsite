b32d69dd97ea504645692762a8ccd048a7385761
Zane DuFour <zane.dufour@gmail.com>
2020-04-19 18:13:58 -0400

    add set_password and check_password

--- znd4/models.py
+++ znd4/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 from znd4 import db
 
 
@@ -8,6 +9,12 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship("Post", backref="author", lazy="dynamic")
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"<User {self.username}>"
