ce368cce3bfecc495fcc11ba38e348b334f5554e
Zane DuFour <zane.dufour@gmail.com>
2020-04-19 12:23:07 -0400

    Finish setting up User model

--- znd4/models.py
+++ znd4/models.py
@@ -3,3 +3,9 @@ from znd4 import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return f"<User {self.username}>"
