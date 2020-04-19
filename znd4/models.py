638c7bf934a6036b4cc216d5ea20740417ce5f96
Zane DuFour <zane.dufour@gmail.com>
2020-04-19 16:55:49 -0400

    add Posts model

--- znd4/models.py
+++ znd4/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from znd4 import db
 
 
@@ -6,6 +7,14 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(120), index=True, unique=True)
+    posts = db.relationship("Post", backref="author", lazy="dynamic")
 
     def __repr__(self):
         return f"<User {self.username}>"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140), primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
