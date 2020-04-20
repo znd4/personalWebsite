eeb0323b4e246e83f0ffe84bf4eae0cdd3dd9021
Zane DuFour <zane.dufour@gmail.com>
2020-04-19 21:51:09 -0400

    Add login handler

--- znd4/routes.py
+++ znd4/routes.py
@@ -3,6 +3,8 @@ This is where we define URLs (routes)
 """
 # pylint: disable=cyclic-import
 from flask import render_template
+from flask_login import current_user, login_user
+from app.models import User
 from znd4 import app
 
 
@@ -17,6 +19,22 @@ def index():
 def resume():
     """The link to my resume"""
     return render_template("resume.html", title="Resume")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Handle log-ins due to"""
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for("login"))
+    return render_template("login.html", title="Sign In", form=form)
 
 
 @app.route("/blog")
