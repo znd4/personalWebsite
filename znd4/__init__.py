c9b548edfb36a6094a08b2514df2b881d822a6e8
Zane DuFour <zane.dufour@gmail.com>
2020-04-19 20:56:49 -0400

    add login to __init__.py

--- znd4/__init__.py
+++ znd4/__init__.py
@@ -6,11 +6,13 @@ although maybe we should be doing that somewhere else.
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+login = LoginManager(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
