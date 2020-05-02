319c93736e4e6fa5cb340ab90db1f7ff0024629a
Zane DuFour <zane.dufour@gmail.com>
2020-05-02 18:13:53 -0400

    add __repr__ to Post

--- znd4/models.py
+++ znd4/models.py
@@ -31,3 +31,6 @@ class Post(db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f"<Post {self.body}>"
