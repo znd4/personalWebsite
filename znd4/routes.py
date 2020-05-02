767a8749eb005c14bd1457574faf97c8843c1a2e
Zane DuFour <zane.dufour@gmail.com>
2020-05-02 19:37:42 -0400

    Add next_page logic for handling logic redirect due to login_required decorator

--- znd4/routes.py
+++ znd4/routes.py
@@ -2,8 +2,9 @@
 This is where we define URLs (routes)
 """
 # pylint: disable=cyclic-import
-from flask import render_template, redirect, flash, url_for
+from flask import render_template, redirect, flash, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
 from znd4.models import User
 from znd4.forms import LoginForm
 from znd4 import app
@@ -35,7 +36,10 @@ def login():
             flash("Invalid username or password")
             return redirect(url_for("login"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("login"))
+        next_page = request.args.get("next")
+        if not next_page or url_parse(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
 
