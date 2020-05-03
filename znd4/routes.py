3dc3ed6c6af2a0af4a52275a9c8d997f807fa7ab
Zane DuFour <zane.dufour@gmail.com>
2020-05-02 21:39:50 -0400

    add user route

--- znd4/routes.py
+++ znd4/routes.py
@@ -36,6 +36,17 @@ def register():
     return redirect(url_for("index"))
 
 
+@app.route("/user/<username>")
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {"author": user, "body": "Test Post #1"},
+        {"author": user, "body": "Test Post #2"},
+    ]
+    return render_template("user.html", user=user, posts=posts)
+
+
 @app.route("/resume")
 def resume():
     """The link to my resume"""
