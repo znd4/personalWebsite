ad9f22a398d8289a29da0ddff851378753959dff
Zane DuFour <zane.dufour@gmail.com>
2020-05-02 20:45:41 -0400

    Add RegistrationForm

--- znd4/forms.py
+++ znd4/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
+from znd4.models import User
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,20 @@ class LoginForm(FlaskForm):
     password = PasswordField("Password", validators=[DataRequired()])
     remember_me = BooleanField("Remember Me")
     submit = SubmitField("Sign In")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    password2 = PasswordField(
+        "Repeat Password", validators=[DataRequired(), EqualTo("password")]
+    )
+
+    def validate_username(self, username):
+        if User.query.filter_by(username=username.data).first():
+            raise ValidationError("That username already exists.")
+
+    def validate_email(self, email):
+        if User.query.filter_by(email=email.data).first():
+            raise ValidationError("That email already exists.")
