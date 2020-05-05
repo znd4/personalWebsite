aa802be11a4d96724a936dacfe338265d93ca014
Zane DuFour <zane.dufour@gmail.com>
2020-05-04 20:53:27 -0400

    db.Datetime -> db.DateTime

--- znd4/models.py
+++ znd4/models.py
@@ -17,7 +17,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship("Post", backref="author", lazy="dynamic")
     about_me = db.Column(db.String(140))
-    last_seen = db.Column(db.Datetime, default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
